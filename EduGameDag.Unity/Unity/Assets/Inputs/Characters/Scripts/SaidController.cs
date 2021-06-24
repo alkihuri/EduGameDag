d9c8ff7ccbc844ef08882c62de5faa6872f51d76
Muhammad Omarov <alkihuri@gmail.com>
2021-06-24 13:01:32 +0300

    MVP есть я какой лев

--- EduGameDag.Unity/Unity/Assets/Inputs/Characters/Scripts/SaidController.cs
+++ EduGameDag.Unity/Unity/Assets/Inputs/Characters/Scripts/SaidController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class SaidController : MonoBehaviour
 {
+    public bool isGameOn;
+    public bool isJump;
+    [SerializeField, Range(0, 10)] float speedOftransition = 5 ; 
+    [SerializeField, Range(0, 4)]  int currentRoad;
+    [SerializeField] List<Transform> roads; // have to be serialized
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("StartGame");
     }
-
+    IEnumerator StartGame()
+    {
+        yield return new WaitForSeconds(3);
+        isGameOn = true;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if(isGameOn)
+           transform.position = Vector3.MoveTowards(transform.position, roads[currentRoad].position + new Vector3(0,1,0), speedOftransition / 10);
     }
 }
