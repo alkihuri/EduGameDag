3cdc06d70c79ef7c729d820ff8750c111a5f06c0
Abdulkadir Karagishiev <kadyrkaragishiev@gmail.com>
2021-08-08 10:53:20 +0300

    Fix Folder & add json

---
+++ EduGameDag.Unity/EduGameDag/Assets/Scripts/GameCore/QuestContrainers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Quest
+{
+    public string question;
+    public string[] wrong_answers;
+    public string right_answer;
+}
+[System.Serializable]
+public class QuestPack
+{
+    public Quest[] quests;
+    public int questCount; //TODO: Create smth for this
+}
