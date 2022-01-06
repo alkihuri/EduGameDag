5fd3dfa21108d2e6802fbd8a6ca3e544f4528511
alkihuri <alkihuri@gmail.com>
2022-01-07 00:27:20 +0200

    Реализовал два круга для букв
    По сути ответ без подсказки  обозначается в текстовом файлу нужным префикмои

--- EduGameDag.Unity/EduGameDag/Assets/Scripts/Entities/Ajdaha/AjdahaDistanceController.cs
+++ EduGameDag.Unity/EduGameDag/Assets/Scripts/Entities/Ajdaha/AjdahaDistanceController.cs
@@ -70,16 +70,12 @@ namespace Entities.Ajdaha
                       + "[" + Time.time.ToString("0.0") + "] ") ;
             if(QuestionGenerator.Instance.QuestionCount == 0)
                 return;
-            needDistance = (QuestionGenerator.Instance.QuestionCount - ScoreController.instance.Score) * 5-2;
+            needDistance = (QuestionGenerator.Instance.QuestionCount - ScoreController.instance.Score) * 5;
             var increaseCount = 0f;
             if ((previousDistance-needDistance) > 0)
             {
                 increaseCount = (100f / QuestionGenerator.Instance.QuestionCount / 100f);
-            }
-            else if ((previousDistance-needDistance) < 0)
-            {
-                //increaseCount = -(100f / QuestionGenerator.Instance.QuestionCount / 100f);
-            }
+            } 
             else
             {
                 Debug.Log("increase = 0");
